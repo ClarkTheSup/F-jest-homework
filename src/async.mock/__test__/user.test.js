8a68ee5d20507d0614d92122768d531f67ab8cfd
ClarkTheSup <lknkoqq123@163.com>
2020-09-17 11:21:36 +0800

    [kangning] fix: mock dependencies

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
-jest.mock("../verify", () => ({
-  ...jest.requireActual("../verify"),
-}));
+jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
   test("should post user when validated", async () => {
     // TODO 19: add test here
     axios.post.mockResolvedValue({ data: "success" });
-    await expect(register("lkn", "123")).resolves.toEqual("success");
+    await expect(register()).resolves.toEqual("success");
     expect(axios.post).toHaveBeenCalledTimes(1);
   });
 
   test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
-    await expect(() => register(null, null)).rejects.toThrowError(
+    verifyUsername.mockReturnValue(false);
+    await expect(() => register()).rejects.toThrowError(
       "wrong username or password"
     );
   });
--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import VaccineTest from "../vaccineTest";
 import Recipient from "../recipient";
 import Covid19Vaccine from "../covid19Vaccine";
@@ -7,13 +6,8 @@ jest.mock("../recipient", () => {
   // mock class实现
   return jest.fn().mockImplementation(() => {
     return {
-      acceptInjection: jest.fn((vaccine) => {
-        if (vaccine.composition.includes("Virus Proteins")) {
-          return true;
-        }
-        return false;
-      }),
-      getHasAntibodies: jest.fn((hasAntibodies) => hasAntibodies),
+      acceptInjection: jest.fn(),
+      getHasAntibodies: jest.fn(),
     };
   });
 });
@@ -27,8 +21,8 @@ describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
     // TODO 14: add test here
     const recipient = new Recipient();
-    const result = recipient.acceptInjection(new Covid19Vaccine());
-    expect(result).toBe(true);
+    recipient.acceptInjection(new Covid19Vaccine());
+    expect(recipient.acceptInjection).toHaveBeenCalledTimes(1);
   });
 });
 
@@ -36,14 +30,18 @@ describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
     // TODO 15: add test here
     const recipient = new Recipient();
-    const hasAntibodies = true;
-    expect(recipient.getHasAntibodies(hasAntibodies)).toBe(true);
+    recipient.getHasAntibodies.mockReturnValueOnce(true);
+    const vaccineTest = new VaccineTest();
+    vaccineTest.recipient = recipient;
+    expect(vaccineTest.test()).toBe("Vaccine Test Success");
   });
 
   test("should get Failed if recipient has no antibodies", () => {
     // TODO 16: add test here
     const recipient = new Recipient();
-    const hasAntibodies = false;
-    expect(recipient.getHasAntibodies(hasAntibodies)).toBe(false);
+    recipient.getHasAntibodies.mockReturnValueOnce(false);
+    const vaccineTest = new VaccineTest();
+    vaccineTest.recipient = recipient;
+    expect(vaccineTest.test()).toBe("Vaccine Test Failed");
   });
 });
